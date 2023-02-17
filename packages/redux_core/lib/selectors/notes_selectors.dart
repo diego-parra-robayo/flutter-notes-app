84f2e8b13ca6aa504c97efcc13ce2a2af7e32bc9
Diego Parra <diego.parra@wizeline.com>
2023-02-17 12:56:33 -0500

    fix: let loading indicator be shown on home screen when a loading action on note is added.

--- packages/redux_core/lib/selectors/notes_selectors.dart
+++ packages/redux_core/lib/selectors/notes_selectors.dart
@@ -1,10 +1,9 @@
 import 'package:core/extensions/list_extensions.dart';
-import 'package:redux_core/entities/entities.dart';
-import 'package:redux_core/store/app_state.dart';
-import 'package:redux_core/state/notes_state.dart';
+import 'package:redux_core/redux_core.dart';
 
 bool selectNotesIsLoading(AppState state) =>
-    state.notes.status == NotesStatus.loading;
+    state.notes.status == NotesStatus.loading ||
+    state.noteDetails.status == NoteDetailsStatus.loading;
 
 List<Note> selectNotes(AppState state) => state.notes.data;
 
